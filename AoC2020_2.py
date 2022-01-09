7a880fec43984332cb324a8a5edb6687ce6b748b
Gillian Law <gillianlaw@techliterate.co.uk>
2022-01-09 13:17:06 +0100

    tidied code, created a function and added comments

--- AoC2020_2.py
+++ AoC2020_2.py
@@ -1,58 +1,36 @@
-import time
 
+def passwordcheck(passrule):
+    total = 0
+    """First, split the line into useful pieces. I could probably do this on
+        one line, but it follows the way my brain thinks (slowly!), and it works.."""
+    for line in passrule:
+        i = line.split(':')
+        j = i[0].split(' ')
+        k = j + i
+
+        count = 0
+        g = k[0].split()
+        if g[0][1] == '-':
+            h = int(g[0][0])
+        else:
+            h = int(g[0][0:2])
+        if g[0][-2:-1] == '-':
+            hh = int(g[0][-1:])
+        else:
+            hh = int(g[0][-2:])
+
+            """Check whether the letter is in the password, and how often"""
+        for letter in k[3]:
+            if letter == k[1]:
+                count +=1
+                """Does it appear within the range of times allowed?"""
+        if count in range(h,hh+1):
+            total += 1
+        print("Total:", total)
 
 with open('data.txt') as f:
     change = [line.rstrip() for line in f]
 
+passwordcheck(change)
 
-
-if __name__ == "__main__":
-  replit_start_time = time.perf_counter()
-
-total = 0
-for line in change:
-
-    i = line.split(':')
-  # print(i)
-    j = i[0].split(' ')
-  # print(j)
-    k = j + i
-  # print("K is ", k)
-
-    count = 0
-
-
-    g = k[0].split()
-    # print("G: ", g)
-
-    if g[0][1] == '-':
-        h = int(g[0][0])
-    else:
-        h = int(g[0][0:2])
-    print("h: ", h)
-
-    if g[0][-2:-1] == '-':
-        hh = int(g[0][-1:])
-    else:
-        hh = int(g[0][-2:])
-    print("hh: ", hh)
-    for letter in k[3]:
-        if letter == k[1]:
-            count +=1
-
-    print('Count:', count)
-
-    # gg = range(h,hh+1)
-
-    if count in range(h,hh+1):
-        total += 1
-        print('Total:', total)
-
-# This doesn't work, I think, when the start and finish numbers are double digit.
-
-
-
-
-
-replit_end_time = time.perf_counter()
-print("Elapsed time:", replit_end_time - replit_start_time)
+# if __name__ == "__main__":
--- AoC2020_2_2.py
+++ AoC2020_2_2.py
@@ -1,53 +1,36 @@
-import time
+
+def passwordcheck(passrule):
+    total = 0
+    """First, split the line into useful pieces. I could probably do this on
+        one line, but it follows the way my brain thinks (slowly!), and it works.."""
+    for line in passrule:
+        i = line.split(':')
+        j = i[0].split(' ')
+        k = j + i
+
+        count = 0
+        g = k[0].split()
+        if g[0][1] == '-':
+            h = int(g[0][0])
+        else:
+            h = int(g[0][0:2])
+        if g[0][-2:-1] == '-':
+            hh = int(g[0][-1:])
+        else:
+            hh = int(g[0][-2:])
+
+            """Does letter appear in those precise spots?"""
+        if k[1] == k[3][h] or k[1] == k[3][hh]:
+            count += 1
+        if k[1] == k[3][h] and k[1] == k[3][hh]:
+            count -= 1
+
+        if count == 1:
+            total += 1
+            print('Total Part 2:', total)
 
 
 with open('data.txt') as f:
     change = [line.rstrip() for line in f]
 
-
-
-if __name__ == "__main__":
-  replit_start_time = time.perf_counter()
-
-total = 0
-for line in change:
-    i = line.split(':')
-    j = i[0].split(' ')
-    k = j + i
-
-    count = 0
-
-    g = k[0].split()
-    if g[0][1] == '-':
-        h = int(g[0][0])
-    else:
-        h = int(g[0][0:2])
-
-    if g[0][-2:-1] == '-':
-        hh = int(g[0][-1:])
-    else:
-        hh = int(g[0][-2:])
-
-    # if k[1] == k[3][h] and k[1] == k[3][hh]:
-    #     pass
-    if k[1] == k[3][h] or k[1] == k[3][hh]:
-        count += 1
-    if k[1] == k[3][h] and k[1] == k[3][hh]:
-        count -= 1
-
-
-
-    # print('Count:', count)
-
-
-    if count == 1:
-        total += 1
-        print('Total:', total)
-
-
-
-
-
-
-replit_end_time = time.perf_counter()
-print("Elapsed time:", replit_end_time - replit_start_time)
+passwordcheck(change)
